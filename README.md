799a381104cbf79ce7566b21ea3fa06e93623be8
EduHoff <ehcs.business@gmail.com>
2026-05-17 17:47:22 -0300

    Estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,15 @@
+# jogo_paciencia_java
+
+## Primeira execução / Rebuild
+```
+docker compose up --build
+```
+
+## Iniciar
+```
+docker compose run --rm jogo_paciencia_java
+```
+
+## Encerrar
+```
+docker compose down
---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import utils.ConsoleUtils;
+
+public class Program {
+
+    public static void main(String[] args) {
+        ConsoleUtils.clear();
+        System.out.println("Hello, World!");
+    }
+}
